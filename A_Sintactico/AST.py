d914ef4485671b2f8bc421d0dd8cb3048d64573a
kukinisVO <ronaldofernandez1@upb.edu>
2025-05-19 14:03:18 -0400

    Adding AST languaje printing

--- A_Sintactico/AST.py
+++ A_Sintactico/AST.py
@@ -19,3 +19,19 @@ class Number(ASTNode):
 class Variable(ASTNode):
     def __init__(self, name):
         self.name = name
+
+
+def print_ast(node, indent=0):
+    indent_str = '  ' * indent
+    if isinstance(node, Assign):
+        print(f"{indent_str}Assign:")
+        print(f"{indent_str}  var: {node.var.name}")
+        print(f"{indent_str}  expr:")
+        print_ast(node.expr, indent + 2)
+    elif isinstance(node, BinOp):
+        print(f"{indent_str}BinOp({node.op}):")
+        print_ast(node.left, indent + 1)
+        print_ast(node.right, indent + 1)
+    elif isinstance(node, (Number, Variable)):
+        print(f"{indent_str}{type(node).__name__}({node.value if isinstance(node, Number) else node.name})")
+
